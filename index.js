dd0eefd09b41fc0cdf4ce9f5bb314bf3af4c5cda
joon-102 <joon_1@outlook.kr>
2024-06-23 17:03:01 +0900

    Add : Header

--- index.js
+++ index.js
@@ -24,6 +24,17 @@ app.use(cors({
     credentials: true,
 }));
 
+app.use(function (req, res, next) {
+    res.setHeader("Access-Control-Allow-Origin", "https://video-class.vercel.app/");
+    res.setHeader("Access-Control-Allow-Methods", "GET, POST");
+    res.setHeader(
+      "Access-Control-Allow-Headers",
+      "X-Requested-With,content-type, Authorization",
+      "Content-Type"
+    );
+    res.setHeader("Access-Control-Allow-Credentials", true);
+    next();
+  });
 
 fs.readdirSync(path.join(process.cwd(), "router")).forEach(file => {
     try {
